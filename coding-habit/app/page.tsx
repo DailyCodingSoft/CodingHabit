25458aea8ca2c0ab2b68152629766a21210785f4
Santiago Rivera Orjuela <dsrivera@homecenter.co>
2026-01-15 22:47:17 -0500

    Agregar estructura inicial de carpetas del frontend

--- coding-habit/app/page.tsx
+++ coding-habit/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <>
-    <h1>Coding Habit 🤑</h1>
+      <h1>Coding Habit 🤑</h1>
+      <p>
+        Una plataforma que permite a los usuarios crear juegos de hábitos usando
+        su trabajo, como crear un compromiso cada día.
+      </p>
     </>
   );
 }
