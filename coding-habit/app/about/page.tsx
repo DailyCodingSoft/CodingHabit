e85ebbb3f03f1c7ec69331870d55450f3090b6cd
Sebastian Lopez <sebas320lopez@hotmail.com>
2026-01-16 12:35:45 -0500

    Se agrega el import de tailwind para que se pueda usar en todo el proyecto

--- coding-habit/app/about/page.tsx
+++ coding-habit/app/about/page.tsx
@@ -1,14 +1,16 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl font-bold mb-2">
+      <h1 className="text-2xl font-bold mb-2 text-center">
         Coding Habit Info
       </h1>
       <div className="mb-6">
-        Empresa dedicada a realizar los mejores proyectos que se te puedan ocurrir.
-        En esta ocasión se tiene la info del equipo.
+        Pequeño proyecot que nacio con el objetivo de desarrollar destresas y habilidades 
+        desarrollando un proyecto que nace de la idea de mmonitorear los cambios  
+        y tiempo que realiza cada uno de los integrantes.
       </div>
-      <div className="lex flex-row bg-red-500">
+      <h1>Integrantes</h1>
+      <div className="flex flex-row bg-red-500">
         <div className="border p-4 flex-1 text-center">
           <h2 className="text-xl font-semibold">Sebastian Lopez</h2>
           <p>Ingeniero Graduado en agosto de 2026</p>
