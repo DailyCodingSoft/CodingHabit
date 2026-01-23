e65c7d75df0194cd323621555543dce4700e2a2a
Sebastian Lopez <sebas320lopez@hotmail.com>
2026-01-23 18:50:54 -0500

    Se agrega metodo para realizar registro de usuarios

--- coding-habit/domain/services/userServices.ts
+++ coding-habit/domain/services/userServices.ts
@@ -7,4 +7,8 @@ export class userService {
         const user = await this.repo.login(email,password)
         return user
     }
+    async register(user_name: string, user_biography:string,github_link:string,user_email: string, user_password: string){
+        const register = await this.repo.register(user_name,user_biography,github_link,user_email,user_password)
+        return register
+    }
 }
