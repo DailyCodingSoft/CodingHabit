fbf5d2970ed1b4fbae0dcd5785f491ce956fd5fc
Sebastian Lopez <sebas320lopez@hotmail.com>
2026-02-10 19:00:34 -0500

    Correccion en peticion de registro

--- coding-habit/domain/services/userServices.ts
+++ coding-habit/domain/services/userServices.ts
@@ -10,8 +10,7 @@ export class userService {
         return user
     }
     async register(user_name: string, user_biography:string,github_link:string,user_email: string, user_password: string){
-        const register = await this.repo.register(user_name,user_biography,github_link,user_email,user_password)
-        return register
+        return await this.repo.register(user_name,user_biography,github_link,user_email,user_password)
     }
     async getById(id:string) {
         const user = await this.repo.getUserById(id);
