8416f60bd8142824314363fffb50c6813285e853
Sebastian Lopez <sebas320lopez@hotmail.com>
2026-02-15 22:03:09 -0500

    Configuracion basica para envio de correos (COMENTADO POR FALTA DE KEY APP AUTENTICANDO)

---
+++ coding-habit/infrastructure/mail/resend.ts
@@ -0,0 +1,24 @@
+/*
+Pendiente de validacion y creacion de la llave 
+https://resend.com/domains/12846f82-84fa-4c7c-8be5-365128d1d6c5
+correo empresaroal
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function sendEmail({
+  to,
+  subject,
+  html,
+}: {
+  to: string;
+  subject: string;
+  html: string;
+}) {
+  return await resend.emails.send({
+    from: "TuApp <noreply@dailycoding.online>",
+    to,
+    subject,
+    html,
+  });
+}*/
