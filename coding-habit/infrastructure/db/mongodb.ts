fd42d997383e01ae90a67369e820e5c6f8b33837
Sebastian Lopez <sebas320lopez@hotmail.com>
2026-04-07 22:09:47 -0500

    db comir

--- coding-habit/infrastructure/db/mongodb.ts
+++ coding-habit/infrastructure/db/mongodb.ts
@@ -13,4 +13,4 @@ attachDatabasePool(client);
 // Export a module-scoped MongoClient to ensure the client can be shared across functions.
 export default client; 
 
-//Comit jueves santo que mi Dios los proteja
+//Comit jueves santo que mi Dios los proteja comir xd
